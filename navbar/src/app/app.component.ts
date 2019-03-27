7fd56dd515432bf33abb10d3a5f22243998957d0
Ransom <spranso@srn.sandia.gov>
2019-03-27 17:16:58 -0600

    added primeng tabs

--- navbar/src/app/app.component.ts
+++ navbar/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TabViewModule} from 'primeng/tabview';
+import { platformCoreDynamicTesting } from '@angular/platform-browser-dynamic/testing/src/platform_core_dynamic_testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'
+]
 })
 export class AppComponent {
   title = 'navbar';
--- navbar/src/app/app.module.ts
+++ navbar/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {TabViewModule} from 'primeng/tabview';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
